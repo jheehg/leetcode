de506aeac3837928c4a6dd67b7dad244580066c7
Ju hee Hong <juhee.hg@gmail.com>
2019-11-01 21:50:26 +0900

    파일 수정

--- firstUniqueCharacterInAString/Solution.java
+++ firstUniqueCharacterInAString/Solution.java
@@ -1,5 +1,28 @@
 package firstUniqueCharacterInAString;
 
-public class Solution {
+class Solution {
+	
+    public int firstUniqChar(String s) {
+        if(s.isEmpty()) return -1;
+        if(s.length() == 1) return 0;
+        
+        int[] alph = new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+            alph[s.charAt(i) - 'a']++;
+        }
+        
+        int min = Integer.MAX_VALUE;
 
+        for(int i=0; i<alph.length; i++){
+            if(alph[i] == 1) {
+                if(min > s.indexOf('a'+i)){
+                    min = s.indexOf('a'+i);
+                }
+            }
+        }
+        
+        return (min > s.length()-1)? -1 : min;
+        
+    }
 }
