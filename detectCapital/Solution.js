6c90a7f76f967bea04ac9985cd45f6d00c7b5229
jheehg <juhee.hg@gmail.com>
2022-07-21 23:59:44 +0900

    # 20220721

---
+++ detectCapital/Solution.js
@@ -0,0 +1,9 @@
+const detectCapitalUse = (word) => {
+  let num = 0;
+  let str = word.slice(1);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] <= 'Z') num++;
+  }
+
+  return (num === str.length && word[0] <= 'Z') || num === 0;
+};
