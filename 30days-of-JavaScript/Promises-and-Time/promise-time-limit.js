45d8568ddc8a1fad259ca983a0838be88974daf3
jheehg <juhee.hg@gmail.com>
2023-06-21 23:45:31 +0900

    add 30days-of-javascript (Promises and Time 2)

---
+++ 30days-of-JavaScript/Promises-and-Time/promise-time-limit.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+    let fn1 = new Promise((_, reject) =>
+      setTimeout(() => {
+        reject('Time Limit Exceeded');
+      }, t)
+    );
+    let fn2 = fn(...args);
+
+    return Promise.race([fn1, fn2]);
+  };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
