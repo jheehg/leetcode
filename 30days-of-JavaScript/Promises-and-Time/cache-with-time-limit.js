628a7db487028b1f3d5a23e30b6c84eead4b62c5
jheehg <juhee.hg@gmail.com>
2023-06-22 23:50:12 +0900

    add 30days-of-javascript (Promises and Time 3)

---
+++ 30days-of-JavaScript/Promises-and-Time/cache-with-time-limit.js
@@ -0,0 +1,45 @@
+var TimeLimitedCache = function () {
+  this.cache = new Map();
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  let result = false;
+  if (this.cache.get(key)) result = true;
+  this.cache.set(key, value);
+
+  setTimeout(() => {
+    if (this.cache.get(key) && this.cache.get(key) === value)
+      this.cache.delete(key);
+  }, duration);
+
+  return result;
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+  return this.cache.get(key) ?? -1;
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
